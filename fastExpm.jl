57388edd43e485631801f172eef39a5de1ee9cdc
fmentink <fmentink@magnet.fsu.edu>
2020-12-05 13:06:14 -0500

    fixed tolerance

--- fastExpm.jl
+++ fastExpm.jl
@@ -20,18 +20,19 @@ using SparseArrays
   - H. J. Hogben, M. Krzystyniak, G. T. P. Charnock, P. J. Hore and I. Kuprov, Spinach – A software library for simulation of spin dynamics in large spin systems, J. Magn. Reson., 2011, 208, 179–194.
   - I. Kuprov, Diagonalization-free implementation of spin relaxation theory for large spin systems., J. Magn. Reson., 2011, 209, 31–38.
 """ 
-function fastExpm(A;threshold=1e-6,nonzero_tol=1e-14)
+function fastExpm(A,threshold=1e-6,nonzero_tol=1e-14)
     mat_norm=norm(A,Inf);
     scaling_factor = nextpow(2,mat_norm); # Native routine, faster
     A = A/scaling_factor;
     delta=1;
 
     # Run Taylor series procedure on the CPU
-    P=sparse(1.0I,size(A)); next_term=P; n=1;
     if nonzeros(A)/length(A)>0.25 || length(A)<64^2
         A=Matrix(A);
+        P=Matrix(1.0I,size(A)); next_term=P; n=1;
     else
         A=sparse(A);
+        P=sparse(1.0I,size(A)); next_term=P; n=1;
     end
 
     while delta>threshold
@@ -65,7 +66,7 @@ function fastExpm(A;threshold=1e-6,nonzero_tol=1e-14)
 end
 
 function droptolerance!(A::Matrix, tolerance)
-    A .= tolerance*round.((1/tolerance)*A)
+    A .= tolerance*round.((1/tolerance).*A)
 end
 function droptolerance!(A::SparseMatrixCSC, tolerance)
     droptol!(A, tolerance) # Native routine, faster
