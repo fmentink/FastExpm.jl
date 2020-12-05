c1151dc179167e1bb6a455ca7630e8bf7a64c532
murari <73845678+eaterofglass@users.noreply.github.com>
2020-12-04 20:39:03 -0500

    Update fastExpm.jl
    The old function failed if the input matrix was non-sparse - A*nextTerm immediately became non-sparse and droptol!() didn't work. I extended the function to non-sparse matrices.

--- fastExpm.jl
+++ fastExpm.jl
@@ -1,12 +1,33 @@
-function taylorExpm(A,threshold=1e-6,nonzero_tol=1e-14)
+using LinearAlgebra
+using SparseArrays
+
+"""
+    fastExpm(A)
+    fastExpm(A; threshold=1e-6, nonzero_tol=1e-14)
+
+ This function efficiently implements matrix exponentiation for sparse and full matrices.
+ This code is based on scaling, taylor series and scaling.
+ Currently works only on the CPU
+
+ Two optional keyword arguments are used to speed up the computation and preserve sparsity.
+ [1] threshold determines the threshold for the Taylor series (default 1e-6)
+ [2] nonzero_tol strips elements smaller than nonzero_tol at each computation step to preserve sparsity (default 1e-14)
+ The code automatically switches from sparse to full if sparsity is below 25% to maintain speed.
+
+ This code was originally developed by Ilya Kuprov (http://spindynamics.org/) and has been adapted by F. Mentink-Vigier (fmentink@magnet.fsu.edu)
+ If you use this code, please cite
+  - H. J. Hogben, M. Krzystyniak, G. T. P. Charnock, P. J. Hore and I. Kuprov, Spinach – A software library for simulation of spin dynamics in large spin systems, J. Magn. Reson., 2011, 208, 179–194.
+  - I. Kuprov, Diagonalization-free implementation of spin relaxation theory for large spin systems., J. Magn. Reson., 2011, 209, 31–38.
+"""
+function fastExpm(A;threshold=1e-6,nonzero_tol=1e-14)
     mat_norm=norm(A,Inf);
-    n_squarings=max(0,ceil(log2(mat_norm))); scaling_factor=2^n_squarings;
-    A=A*scaling_factor^-1;
+    scaling_factor = nextpow(2,mat_norm); # Native routine, faster
+    A = A/scaling_factor;
     delta=1;
 
     # Run Taylor series procedure on the CPU
     P=sparse(I,size(A)); next_term=P; n=1;
-    if sum(x->x>0, abs.(A))/length(A)>0.25 || length(A)<64^2
+    if nonzeros(A)/length(A)>0.25 || length(A)<64^2
         A=Matrix(A);
     else
         A=sparse(A);
@@ -17,9 +38,8 @@ function taylorExpm(A,threshold=1e-6,nonzero_tol=1e-14)
         if issparse(next_term)
             next_term=(1/n)*A*next_term;
             #Eliminate small elements
-            #next_term=nonzero_tol*round.((1/nonzero_tol)*next_term);
-            next_term=droptol!(next_term, nonzero_tol); #julia's internal function
-            if sum(x->x>0, abs.(next_term))/length(next_term)>0.25
+            next_term=droptolerance!(next_term, nonzero_tol);
+            if nonzeros(next_term)/length(next_term)>0.25
                 next_term=Matrix(next_term);
             end
         else
@@ -27,19 +47,32 @@ function taylorExpm(A,threshold=1e-6,nonzero_tol=1e-14)
         end
         delta=norm(next_term,Inf);
         #Add to the total and increment the counter
-        P=P+next_term; n=n+1;
+        P .+= next_term; n=n+1;
     end
     #Squaring of P to generate correct P
-    for n=1:n_squarings
+    for n=1:log2(scaling_factor)
         P=P*P;
         if issparse(P)
-            if sum(x->x>0, abs.(P))/length(P)>0.25
-                #P=nonzero_tol*round.((1/nonzero_tol)*P);
-                P=droptol!(P, nonzero_tol);
+            if nonzeros(P)/length(P)>0.25
+                P = droptolerance!(P, nonzero_tol);
             else
                 P=Matrix(P);
             end
         end
     end
     return P
+end
+
+function droptolerance!(A::Matrix, tolerance)
+    A .= tolerance*round.((1/tolerance)*A)
+end
+function droptolerance!(A::SparseMatrixCSC, tolerance)
+    droptol!(A, tolerance) # Native routine, faster
+end
+
+function nonzeros(A::Matrix)
+    count(x->x>0, abs.(A))
+end
+function nonzeros(A::SparseMatrixCSC)
+    nnz(A) # Native routine, faster
 end
