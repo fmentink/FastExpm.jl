974f25de34a208050d629a0f1169dc81e100b44f
fmentink <fmentink@magnet.fsu.edu>
2020-12-05 13:31:36 -0500

    More explicit documentartion for matlab users

--- fastExpm.jl
+++ fastExpm.jl
@@ -10,8 +10,8 @@ using SparseArrays
  Currently works only on the CPU
 
  Two optional keyword arguments are used to speed up the computation and preserve sparsity.
- [1] threshold determines the threshold for the Taylor series (default 1e-6)
- [2] nonzero_tol strips elements smaller than nonzero_tol at each computation step to preserve sparsity (default 1e-14)
+ [1] threshold determines the threshold for the Taylor series (default 1e-6): e.g. fastExpm(A, threshold=1e-10) 
+ [2] nonzero_tol strips elements smaller than nonzero_tol at each computation step to preserve sparsity (default 1e-14) ,e.g. fastExpm(A, nonzero_tol=1e-10) 
  The code automatically switches from sparse to full if sparsity is below 25% to maintain speed.
 
  This code was originally developed by Ilya Kuprov (http://spindynamics.org/) and has been adapted by F. Mentink-Vigier (fmentink@magnet.fsu.edu)
